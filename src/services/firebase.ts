793fbda279f1d6dd23fc8cf42afea7cb787037bd
weescoelho <wes.coelho18@gmail.com>
2021-06-20 20:55:59 -0300

    refactor(firebase.ts): configuration .env variables  on firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0eglKnQTxAmjaFIX27I6F3OV93aS8Ha8",
-  authDomain: "letmeask-9935e.firebaseapp.com",
-  databaseURL: "https://letmeask-9935e-default-rtdb.firebaseio.com",
-  projectId: "letmeask-9935e",
-  storageBucket: "letmeask-9935e.appspot.com",
-  messagingSenderId: "582599548463",
-  appId: "1:582599548463:web:002505e4361daac379b0da"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID
 };
 
 firebase.initializeApp(firebaseConfig)
