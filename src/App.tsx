8fed1d7beae83708dff7ff9f3b24432a6691553e
weescoelho <wes.coelho18@gmail.com>
2021-06-21 20:06:37 -0300

    feat: add theming structure for swiching

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import React from 'react';
+import { ThemeProvider, DefaultTheme } from 'styled-components'
+import usePersistedState from './utils/usePersistedState';
+import light from './styles/theme/light'
+import dark from './styles/theme/dark'
+import GlobalStyle from './styles/global'
+
 function App() {
+
+  const [theme,setTheme] = usePersistedState<DefaultTheme>('theme',light)
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light)
+  }
+
   return (
-   <h1>Hello World</h1>
+    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+    </ThemeProvider>
+    </>
   );
 }
 
