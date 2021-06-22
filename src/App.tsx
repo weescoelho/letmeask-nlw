13cf3f941465cd03ff016439cbd6382f354e02b4
weescoelho <wes.coelho18@gmail.com>
2021-06-22 00:11:45 -0300

    feat: configuration react-router-dom, implement authcontext

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import { ThemeProvider, DefaultTheme } from 'styled-components'
-import usePersistedState from './utils/usePersistedState';
-import light from './styles/theme/light'
-import dark from './styles/theme/dark'
-import GlobalStyle from './styles/global'
+import React from "react";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import usePersistedState from "./utils/usePersistedState";
+import light from "./styles/theme/light";
+import dark from "./styles/theme/dark";
+import GlobalStyle from "./styles/global";
+import Home from "./pages/Home/Home";
+import NewRoom from "./pages/NewRoom/NewRoom";
+
+import { BrowserRouter, Route } from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
-
-  const [theme,setTheme] = usePersistedState<DefaultTheme>('theme',light)
+  const [theme, setTheme] = usePersistedState<DefaultTheme>("theme", light);
   const toggleTheme = () => {
-    setTheme(theme.title === 'light' ? dark : light)
-  }
+    setTheme(theme.title === "light" ? dark : light);
+  };
 
   return (
     <>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle/>
-    </ThemeProvider>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Route path="/" exact component={Home} />
+            <Route path="/rooms/new" component={NewRoom} />
+          </ThemeProvider>
+        </AuthContextProvider>
+      </BrowserRouter>
     </>
   );
 }
