7fde6c2ae53d1deb403fb77f3948e64f42072b25
weescoelho <wes.coelho18@gmail.com>
2021-06-22 23:14:14 -0300

    feat: created inputtextarea component

---
+++ src/components/InputTextarea.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+type Props = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+const Textarea = styled.textarea`
+  width: 100%;
+  border: 0;
+  padding: 16px;
+  border-radius: 8px;
+  background: ${({ theme }) => theme.colors.secondary};
+  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.04);
+  resize: vertical;
+  min-height:130px;
+`;
+
+const InputTextarea: React.FC<Props> = ({ ...props }) => {
+  return <Textarea {...props} />;
+};
+
+export default InputTextarea;
