5a5c25abbbe60ff99332ea8457dad1e6a145a8d3
weescoelho <wes.coelho18@gmail.com>
2021-06-22 23:15:15 -0300

    feat: created question counter component

---
+++ src/components/QuestionsCounter.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.span`
+  margin-left: 16px;
+  background: #e559f9;
+  border-radius: 9999px;
+  padding: 8px 16px;
+  color: #fff;
+`;
+
+type Props = {
+  questions: number;
+};
+
+const QuestionsCounter = ({ questions }: Props) => {
+  return (
+    <Container>
+      {questions} {questions > 1 ? "perguntas" : "pergunta"}
+    </Container>
+  );
+};
+
+export default QuestionsCounter;
