fb9dd99c8bd6da5d5d69178fbc59695f1f4aac45
weescoelho <wes.coelho18@gmail.com>
2021-06-23 23:18:43 -0300

    refactor: add new colors

--- src/styles/theme/dark.ts
+++ src/styles/theme/dark.ts
@@ -1,9 +1,10 @@
 export default {
   title: 'dark',
   colors: {
-    primary: '#4a148c',
+    primary: '#51BBFE',
     secondary: '#333',
     background: "#222",
-    text: '#F8F8F8'
+    text: '#F8F8F8',
+    gray:'#F8F8F8',
   }
 }
--- src/styles/theme/light.ts
+++ src/styles/theme/light.ts
@@ -1,9 +1,10 @@
 export default {
   title: 'light',
   colors: {
-    primary: '#835AFD',
+    primary: '#51BBFE',
     secondary: '#FFFFFF',
     background: "#F8F8F8",
-    text: '#29292E'
+    text: '#29292E',
+    gray:'#737380'
   }
 }
