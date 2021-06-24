6e2a0e2a609f319f709bfe55d2d7b98cce6bfab1
weescoelho <wes.coelho18@gmail.com>
2021-06-23 23:22:08 -0300

    feat: created useRoom hook

---
+++ src/hooks/useRoom.ts
@@ -0,0 +1,72 @@
+/**
+ * Custom hook para abstração das funcionalidades de carregamento das perguntas
+ * 
+ */
+
+import React from 'react'
+import { database } from '../services/firebase';
+import { useAuth } from './useAuth';
+
+type QuestionsType = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isHighlighted: boolean;
+  isAnswered: string;
+  likeCount: number;
+  likeId: string | undefined;
+};
+
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isHighlighted: boolean;
+    isAnswered: string;
+    likes: Record<string, { authorId: string }>
+  }
+>;
+
+export const useRoom = (roomId: string) => {
+  const { user } = useAuth()
+  const [questions, setQuestions] = React.useState<QuestionsType[]>([]);
+  const [title, setTitle] = React.useState("");
+
+  React.useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+    roomRef.on("value", (room) => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+      const parsedQuestion = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHighlighted,
+            isAnswered: value.isAnswered,
+            likeCount: Object.values(value.likes ?? {}).length,
+            likeId: Object.entries(value.likes ?? {}).find(([key,like]) => like.authorId === user?.id)?.[0]
+          };
+        },
+      );
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestion);
+    });
+    return () => {
+      roomRef.off('value')
+    }
+  }, [roomId, user?.id]);
+
+  return {
+    questions, title
+  }
+
+}
