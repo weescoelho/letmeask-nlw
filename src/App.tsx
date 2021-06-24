51239d354da250ab0665713d11cbd4eda74b4f7a
weescoelho <wes.coelho18@gmail.com>
2021-06-23 23:23:56 -0300

    feat: add admin room page on imports

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Room from "./pages/Room/Room";
 import { BrowserRouter, Route, Switch as Routes } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext";
 import { ThemeContext } from "./contexts/ThemeContext";
+import AdminRoom from "./pages/AdminRoom/AdminRoom";
 
 function App() {
   const { theme } = React.useContext(ThemeContext);
@@ -21,6 +22,7 @@ function App() {
               <Route path="/" exact component={Home} />
               <Route path="/rooms/new" component={NewRoom} />
               <Route path="/rooms/:id" component={Room} />
+              <Route path="/admin/rooms/:id" component={AdminRoom} />
             </Routes>
           </ThemeProvider>
         </AuthContextProvider>
