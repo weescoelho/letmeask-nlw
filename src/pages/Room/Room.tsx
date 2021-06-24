98bd3688e2cb1de18c26637467e0d2586ef5f273
weescoelho <wes.coelho18@gmail.com>
2021-06-23 23:23:29 -0300

    feat: abstraction types and component to useRoom hook

--- src/pages/Room/Room.tsx
+++ src/pages/Room/Room.tsx
@@ -3,10 +3,12 @@ import { useParams } from "react-router-dom";
 import Button from "../../components/Button";
 import Header from "../../components/Header";
 import InputTextarea from "../../components/InputTextarea";
+import Question from "../../components/Question";
 import QuestionsCounter from "../../components/QuestionsCounter";
 import RoomTitle from "../../components/RoomTitle";
 import UserInfo from "../../components/UserInfo";
 import { useAuth } from "../../hooks/useAuth";
+import { useRoom } from "../../hooks/useRoom";
 import { database } from "../../services/firebase";
 import {
   Container,
@@ -15,32 +17,8 @@ import {
   FormFooter,
 } from "./Room.styled";
 
-type FirebaseQuestions = Record<
-  string,
-  {
-    author: {
-      name: string;
-      avatar: string;
-    };
-    content: string;
-    isHighlighted: boolean;
-    isAnswered: string;
-  }
->;
-
 type RoomParams = {
   id: string;
-};
-
-type Questions = {
-  id: string;
-  author: {
-    name: string;
-    avatar: string;
-  };
-  content: string;
-  isHighlighted: boolean;
-  isAnswered: string;
 };
 
 const Room = () => {
@@ -48,29 +26,7 @@ const Room = () => {
   const params = useParams<RoomParams>();
   const roomId = params.id;
   const [newQuestion, setNewQuestion] = React.useState("");
-  const [questions, setQuestions] = React.useState<Questions[]>([]);
-  const [title, setTitle] = React.useState("");
-
-  React.useEffect(() => {
-    const roomRef = database.ref(`rooms/${roomId}`);
-    roomRef.on("value", (room) => {
-      const databaseRoom = room.val();
-      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
-      const parsedQuestion = Object.entries(firebaseQuestions).map(
-        ([key, value]) => {
-          return {
-            id: key,
-            content: value.content,
-            author: value.author,
-            isHighlighted: value.isHighlighted,
-            isAnswered: value.isAnswered,
-          };
-        },
-      );
-      setTitle(databaseRoom.title);
-      setQuestions(parsedQuestion);
-    });
-  }, [roomId]);
+  const { questions, title } = useRoom(roomId);
 
   const handleSendQuestion = async (event: FormEvent) => {
     event.preventDefault();
@@ -91,11 +47,13 @@ const Room = () => {
 
   return (
     <Container>
-      <Header />
+      <Header admin={false} />
       <Content>
         <RoomTitleContainer>
           <RoomTitle title={title} />
-          {questions.length > 0 && (<QuestionsCounter questions={questions.length} />)}
+          {questions.length > 0 && (
+            <QuestionsCounter questions={questions.length} />
+          )}
         </RoomTitleContainer>
         <form onSubmit={handleSendQuestion}>
           <InputTextarea
@@ -116,6 +74,19 @@ const Room = () => {
             </Button>
           </FormFooter>
         </form>
+        {questions.map((question) => (
+          <Question
+            key={question.id}
+            content={question.content}
+            author={question.author}
+            admin={false}
+            questionId={question.id}
+            roomId={roomId}
+            user={user}
+            likeCount={question.likeCount}
+            likeId={question.likeId}
+          />
+        ))}
       </Content>
     </Container>
   );
