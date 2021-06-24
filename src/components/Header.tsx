18c6e7c4f604b53faf953656fc08ff9e43b71864
weescoelho <wes.coelho18@gmail.com>
2021-06-23 23:20:13 -0300

    feat: new functions and admin mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,55 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import styled from "styled-components";
 import { ThemeContext } from "../contexts/ThemeContext";
+import useMedia from "../hooks/useMedia";
+import { database } from "../services/firebase";
+import Button from "./Button";
 import Logo from "./Logo";
 import RoomCode from "./RoomCode";
 import SwitchButton from "./SwitchButton";
 
 type RoomParams = {
   id: string;
-}
+};
 
-const Header = () => {
+type HeaderProps = {
+  admin: boolean;
+  roomId?:string;
+};
+
+const Header = ({admin,roomId}: HeaderProps) => {
+  const history = useHistory();
   const { toggleTheme } = React.useContext(ThemeContext);
   const params = useParams<RoomParams>();
+  const mobile = useMedia('(max-width:739px)')
+
+  const handleEndRoom = async () => {
+    await database.ref(`rooms/${roomId}`).update({
+      endedAt:new Date(),
+    })
+    history.push('/')
+  }
 
   return (
     <Container>
       <Content>
-        <Logo width={"100"} />
+        <LogoWrapper>
+          <Logo width={"100"} />
+          {mobile && <SwitchButton toggleTheme={toggleTheme} />}
+        </LogoWrapper>
         <Wrapper>
-          <RoomCode code={params.id}/>
-          <SwitchButton toggleTheme={toggleTheme}/>
+          <RoomCode code={params.id} />
+          {admin && (
+            <Button
+              outline
+              style={{ width: "160px", height: "40px"}}
+              onClick={handleEndRoom}
+            >
+              Encerrar sala
+            </Button>
+          )}
+          {!mobile && <SwitchButton toggleTheme={toggleTheme} />}
         </Wrapper>
       </Content>
     </Container>
@@ -30,7 +59,7 @@ const Header = () => {
 const Container = styled.header`
   padding: 24px 0 0 0;
   border-bottom: 1px solid ${({ theme }) => theme.colors.secondary};
-  @media (max-width:739px){
+  @media (max-width: 739px) {
     padding: 24px 0 24px 0;
   }
 `;
@@ -41,11 +70,11 @@ const Content = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  @media (max-width:739px){
+  @media (max-width: 739px) {
     max-width: 290px;
     flex-direction: column;
   }
-  @media (min-width:739px) and (max-width:989px){
+  @media (min-width: 739px) and (max-width: 989px) {
     max-width: 640px;
   }
 `;
@@ -54,6 +83,14 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  gap:16px;
+`;
+
+const LogoWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export default Header;
